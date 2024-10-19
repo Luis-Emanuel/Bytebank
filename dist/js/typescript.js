ad1b9b370013afebc99eebef71585ec5af48847f
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2024-10-19 20:14:43 -0300

    Organizados arquivos
    Organizei os arquivos dessa forma: 
    Uma pasta destinada à produção, ou seja, ao ambiente de produção, onde a aplicação será realmente rodada, o que vai realmente para o servidor para ser hospedado — no caso de projetos web;
    Uma pasta de desenvolvimento, com arquivos que só interessam no momento de desenvolvimento do código, isto é, da escrita do código.

---
+++ dist/js/typescript.js
@@ -0,0 +1,12 @@
+let valor = 3000;
+let nome = "";
+let isPago = false;
+let qualquer = "";
+qualquer = 22;
+const lista = [];
+lista.push(13, 22.5, 22, 89, 1.58);
+const novaTransacao = {
+    tipoTransacao: "",
+    data: new Date,
+    valor: 0
+};
